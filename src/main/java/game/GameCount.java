b795ddbe0eb92b4d0b078a8f6c9ae9b4ea30e318
Anony2730677mous <104105@mail.ru>
2022-12-03 19:09:49 +0300

    v_5 Fix GameCount and add new Test

--- src/main/java/game/GameCount.java
+++ src/main/java/game/GameCount.java
@@ -7,19 +7,30 @@ public class GameCount
     {
 
         private final Map<String, Integer> map;
+        private final String nullMessage = "Входящий аргумент = null";
         GameCount()
         {
             map = new HashMap<>();
         }
         public void addUser(String name)
         {
+            if(name == null)
+            {
+                throw new IllegalArgumentException(nullMessage);
+            }
             if(!map.containsKey(name))
             {
                 map.put(name, 0);
             }
+
+
         }
         public Integer getGameCount(String name)
         {
+            if(name == null)
+            {
+                throw new IllegalArgumentException(nullMessage);
+            }
             Integer gameCount = 0;
             if(map.containsKey(name))
             {
@@ -30,6 +41,10 @@ public class GameCount
 
         public void setGameCount(String name, Integer gameCount)
         {
+            if(name == null || gameCount == null)
+            {
+                throw new IllegalArgumentException(nullMessage);
+            }
             if(map.containsKey(name))
             {
                 map.replace(name, map.get(name), gameCount);
@@ -38,6 +53,10 @@ public class GameCount
 
         public boolean getThisUser(String name)
         {
+            if(name == null)
+            {
+                throw new IllegalArgumentException(nullMessage);
+            }
             return map.containsKey(name);
         }
 
--- src/test/java/game/GameLogicTest.java
+++ src/test/java/game/GameLogicTest.java
@@ -11,6 +11,8 @@ import java.util.List;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class GameLogicTest {
     private GameLogic gameLogic;
+    private final String gameState1 = "Ты потерял память. Принять вызов НЛО?";
+
     @BeforeAll
     void init()
     {
@@ -25,11 +27,12 @@ class GameLogicTest {
     @Test
     void answer_Return_No_Empty_List_Test()
     {
-        init();
         List<String> gameState = new ArrayList<>();
-        gameState.add(gameLogic.answerListGetAnswer("Ты потерял память. Принять вызов НЛО?"));
-        gameState.add(gameLogic.answerListGetAnswer("Ты принял вызов. Поднимешься на мостик к их капитану?"));
-        gameState.add(gameLogic.answerListGetAnswer("Ты поднялся на мостик. Ты кто?"));
+        gameState.add(gameLogic.answerListGetAnswer(gameState1));
+        String gameState2 = "Ты принял вызов. Поднимешься на мостик к их капитану?";
+        gameState.add(gameLogic.answerListGetAnswer(gameState2));
+        String gameState3 = "Ты поднялся на мостик. Ты кто?";
+        gameState.add(gameLogic.answerListGetAnswer(gameState3));
         for(String string: gameState)
         {
             Assertions.assertFalse( gameLogic.answer(string).isEmpty());
@@ -39,11 +42,12 @@ class GameLogicTest {
     void answer_Return_List_Size_Test()
     {
 
-        Assertions.assertEquals(4, gameLogic.answer("Ты потерял память. Принять вызов НЛО?").size());
+        int answerListSize = 4;
+        Assertions.assertEquals(answerListSize, gameLogic.answer(gameState1).size());
     }
     @Test
     void answer_Return_String_Type_Test()
     {
-        Assertions.assertEquals("String", gameLogic.answer("Ты потерял память. Принять вызов НЛО?").get(0).getClass().getSimpleName());
+        Assertions.assertEquals("String", gameLogic.answer(gameState1).get(0).getClass().getSimpleName());
     }
 }
